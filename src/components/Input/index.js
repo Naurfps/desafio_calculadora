f4965f1c6eeb2165f889b4d11960ea59c034f4b2
Bowie <alexcaf7@gmail.com>
2024-03-04 20:32:40 -0300

    commit-do-projeto

---
+++ src/components/Input/index.js
@@ -0,0 +1,11 @@
+import { InputContainer } from "./styled"
+
+const Input = () => {
+  return (
+    <InputContainer>
+      <Input />
+    </InputContainer>
+  )
+}
+
+export default Input
---
+++ src/components/Input/styled.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    background-color: #AAAAFF;
+    height: 75px;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end:
+    font-size: 24px;
+    font-family: "Roboto";
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAAAFF;
+        border: 0;
+        display: flex;
+        align-items: flex-end;
+        font-size: 24px;
+        font-family: "Roboto";
+    }
+`
